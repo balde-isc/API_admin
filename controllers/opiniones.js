872187c45ad08f2cfc7de28d713b913e5dd022f3
Baldemar Guzman <baldemar.isc@gmail.com>
2017-12-05 20:06:33 -0600

    Saco todas las opiniones de lo pedido /get...

--- controllers/opiniones.js
+++ controllers/opiniones.js
@@ -7,15 +7,63 @@ var Opinion = require('../models/opiniones');
   });
 }
 
-function ObtenerOpiniones (req, res){
-  Opinion.find({},(err, opiniones)=>{
+function ObtenerOpinionesLikeSeg (req, res){
+  Opinion.find({ opinion: "like", motivo:"seguridad"},(err, opiniones)=>{
     if(err) return res.status(500).send({message: "erro al pedir opiniones"});
     if(!opiniones) return res.status(402).send({message: "no existen opiniones"});
     
     res.send(200, {opiniones});
-  })
+  });
+}
+function ObtenerOpinionesLikeVia (req, res){
+  Opinion.find({ opinion: "like", motivo:"vialidad"},(err, opiniones)=>{
+    if(err) return res.status(500).send({message: "erro al pedir opiniones"});
+    if(!opiniones) return res.status(402).send({message: "no existen opiniones"});
+    
+    res.send(200, {opiniones});
+  });
+}
+function ObtenerOpinionesLikeInf (req, res){
+  Opinion.find({ opinion: "like", motivo:"infrastructura"},(err, opiniones)=>{
+    if(err) return res.status(500).send({message: "erro al pedir opiniones"});
+    if(!opiniones) return res.status(402).send({message: "no existen opiniones"});
+    
+    res.send(200, {opiniones});
+  });
+}
+function ObtenerOpinionesDisLikeSeg (req, res){
+  Opinion.find({ opinion: "dislike", motivo:"seguridad"},(err, opiniones)=>{
+    if(err) return res.status(500).send({message: "erro al pedir opiniones"});
+    if(!opiniones) return res.status(402).send({message: "no existen opiniones"});
+    
+    res.send(200, {opiniones});
+  });
+}
+function ObtenerOpinionesDisLikeVia (req, res){
+  Opinion.find({ opinion: "dislike", motivo:"vialidad"},(err, opiniones)=>{
+    if(err) return res.status(500).send({message: "erro al pedir opiniones"});
+    if(!opiniones) return res.status(402).send({message: "no existen opiniones"});
+    
+    res.send(200, {opiniones});
+  });
 }
 
+function ObtenerOpinionesDisLikeInf (req, res){
+  Opinion.find({ opinion: "dislike", motivo:"infrastructura"},(err, opiniones)=>{
+    if(err) return res.status(500).send({message: "erro al pedir opiniones"});
+    if(!opiniones) return res.status(402).send({message: "no existen opiniones"});
+    
+    res.send(200, {opiniones});
+  });
+}
+
+
 module.exports = {
-ObtenerOpiniones
+  ObtenerOpinionesLikeSeg,
+  ObtenerOpinionesLikeVia,
+  ObtenerOpinionesLikeInf,
+  ObtenerOpinionesDisLikeSeg,
+  ObtenerOpinionesDisLikeVia,
+  ObtenerOpinionesDisLikeInf
+
 };
--- routes/opiniones.js
+++ routes/opiniones.js
@@ -4,5 +4,12 @@ var express = require('express');
 var OpinionesController = require('../controllers/opiniones');
 var api = express.Router();
 
-api.get('/get', OpinionesController.ObtenerOpiniones);
+api.get('/getLikeSeg', OpinionesController.ObtenerOpinionesLikeSeg);
+api.get('/getLikeVia', OpinionesController.ObtenerOpinionesLikeVia);
+api.get('/getLikeInf', OpinionesController.ObtenerOpinionesLikeInf);
+
+api.get('/getDisLikeSeg', OpinionesController.ObtenerOpinionesDisLikeSeg);
+api.get('/getDisLikeVia', OpinionesController.ObtenerOpinionesDisLikeVia);
+api.get('/getDisLikeinf', OpinionesController.ObtenerOpinionesDisLikeInf);
+
 module.exports = api;
